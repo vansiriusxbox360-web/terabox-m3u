01ea38d2903f83fea92e7fe3c11d270fe1dae0da
VanSirius <vansiriusxbox360-web@users.noreply.github.com>
2026-08-06 00:55:47 +0200

    fix: reproduce.py se mantiene vivo la duracion del WAV (SND_ASYNC moria con el proceso) v1.0.9

--- kodi-addon/service.vansirius.sounds/resources/reproduce.py
+++ kodi-addon/service.vansirius.sounds/resources/reproduce.py
@@ -1,4 +1,16 @@
-import sys, winsound
+import sys
+import wave
+import winsound
+import time
 
 if len(sys.argv) > 1:
-    winsound.PlaySound(sys.argv[1], winsound.SND_FILENAME | winsound.SND_ASYNC)
+    path = sys.argv[1]
+    # Mantener vivo el proceso mientras suena el WAV (SND_ASYNC muere con el proceso)
+    dur = 2.0
+    try:
+        with wave.open(path, 'rb') as w:
+            dur = w.getnframes() / float(w.getframerate()) + 0.5
+    except Exception:
+        pass
+    winsound.PlaySound(path, winsound.SND_FILENAME | winsound.SND_ASYNC)
+    time.sleep(dur)
